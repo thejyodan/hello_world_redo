cd15af846634d57b3360c1cbb39b048cbdd52f4c
Jordan <jyodan90@gmail.com>
2022-10-26 15:52:44 -0700

    Add files via upload
    A redo of hello_world.py Python practice assignment

---
+++ hello_world.py
@@ -0,0 +1,18 @@
+# 1. TASK: print "Hello World"
+# print("Hello World")
+
+# 2. print "Hello Jordan" with the name in a variable
+# name = "Jordan"
+# print("Hello" , name )	# with a comma
+# print( "Hello" + name)	# with a +
+
+# 3. print "Hello 8" with the number in a variable
+# name = 8
+# print( "Hello" , 8)	# with a comma
+
+# 4. print "I love to eat sushi and pizza." with the foods in variables
+# fave_food1 = "sushi"
+# fave_food2 = "pizza"
+# print("I love to eat {fave_food1} and {fave_food2}.").format(fave_food1, fave_food2) # with .format()
+# print(f"I love to eat {fave_food1} and {fave_food2}.") # with an f string
+
